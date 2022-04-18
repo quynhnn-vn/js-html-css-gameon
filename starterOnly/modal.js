f6840ec665c61e7b62c1817355a63ea4f39b3d79
Maxence DEGLAINE <nnquynh.0111@gmail.com>
2022-04-18 09:47:01 +0200

    [Feat]: Finish task 2,3,4

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,22 @@
+// DOM Elements
+const modalBg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".close");
+const form = document.getElementById("form");
+
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birthdayInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
+const checkboxInput = document.getElementById("checkbox1");
+const radioInput = document.querySelectorAll("input[type=radio]");
+const submitBtn = document.querySelector("input[type=submit]");
+
+/**
+ * @desc Set topnav responsive
+ */
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,31 +26,41 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalBg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const modalClose = document.querySelectorAll(".close");
-const form = document.getElementById("form");
-const emailField = document.getElementById("email");
 /**
- * @desc launch modal form
+ * @desc Launch modal form
  */
 function launchModal() {
   modalBg.style.display = "block";
 }
-// launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 /**
- * @desc: close modal form
+ * @desc Close modal form
  */
 function closeModal() {
   modalBg.style.display = "none";
 }
-// close modal event
-modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
+modalClose.addEventListener("click", closeModal);
 
 function submitForm() {
-  form.submit();
+  const isValidText = [
+    firstNameInput,
+    lastNameInput,
+    emailInput,
+    birthdayInput,
+    quantityInput,
+    checkboxInput,
+  ].every((input) => input.checkValidity());
+
+  let isValidRadio = false;
+  for (var i = 0, len = radioInput.length; i < len; i++) {
+    if (radioInput[i].checked) {
+      isValidRadio = true;
+    }
+  }
+
+  if (isValidText && isValidRadio) {
+    alert("Merci ! Votre réservation a été reçue.");
+    form.reset();
+  }
 }
