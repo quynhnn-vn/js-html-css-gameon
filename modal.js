ad28a763c8fcd015cedd506f8b93bacecafe02f5
Maxence DEGLAINE <nnquynh.0111@gmail.com>
2022-05-06 14:47:49 +0200

    [Feat]: Add comment for code

--- modal.js
+++ modal.js
@@ -1,8 +1,9 @@
-// DOM Elements
+/* DOM elements of the inscription modal */
 const modalBg = document.querySelector(".bground");
 const modalBtn = document.querySelector(".modal-btn");
 const modalClose = document.querySelectorAll(".close");
 
+/* DOM elements of all inputs */
 const form = document.getElementById("form");
 const firstNameInput = document.getElementById("first");
 const lastNameInput = document.getElementById("last");
@@ -12,9 +13,11 @@ const quantityInput = document.getElementById("quantity");
 const checkboxInput = document.getElementById("checkbox1");
 const radioInput = document.querySelectorAll("input[type=radio]");
 
+/* DOM elements of the confirmation modal */
 const alertBg = document.querySelector(".alert-bground");
 const alertClose = document.querySelector(".btn-close");
 
+/* DOM elements array of type text/email/date/number inputs */
 const TEXT_INPUTS = [
   firstNameInput,
   lastNameInput,
@@ -31,12 +34,12 @@ const editNav = () => {
     : (x.className = "topnav");
 };
 
-/* Launch form modal */
+/* Launch inscription modal */
 modalBtn.addEventListener("click", () => {
   modalBg.style.display = "block";
 });
 
-/* Close form modal or alert modal */
+/* Close inscription modal or confirmation modal */
 modalClose.forEach((closeBtn) =>
   closeBtn.addEventListener("click", () => {
     modalBg.style.display = "none";
@@ -44,19 +47,21 @@ modalClose.forEach((closeBtn) =>
   })
 );
 
-/* Close alert modal */
+/* Close confirmation modal */
 alertClose.addEventListener("click", () => {
   alertBg.style.display = "none";
 });
 
 /* Show error message */
 const showErrorMessage = (input) => {
+  // Assign message values to inputs
   let message = "";
   if (input === emailInput) message = "un email valid";
   else if (input === birthdayInput) message = "une date valide";
   else if (input === quantityInput) message = "une valeur numérique";
   else message = "ce champ avec un minimum de 2 caractères";
 
+  // Set attributes to change styles of the invalid input
   input.parentElement.setAttribute("data-error-visible", "true");
   input.parentElement.setAttribute(
     "data-error",
@@ -66,6 +71,7 @@ const showErrorMessage = (input) => {
 
 /* Hide error message */
 const hideErrorMessage = (input) => {
+  // Remove attributes to change styles of the valid input
   input.parentElement.removeAttribute("data-error-visible");
   input.parentElement.removeAttribute("data-error");
 };
@@ -79,15 +85,19 @@ const checkValidityBeforeSubmit = (input) => {
     }
   });
 };
+/* Apply checkValidityBeforeSubmit for each text inputs */
 TEXT_INPUTS.forEach((input) => checkValidityBeforeSubmit(input));
 
 /* Check validity and submit form */
 const submitForm = (e) => {
   e.preventDefault();
+
+  // isValidText = true if every text inputs and checkbox are valid
   const isValidText = [...TEXT_INPUTS, checkboxInput].every((input) =>
     input.checkValidity()
   );
 
+  // isValidRadio = true if at least one radio input is checked
   let isValidRadio = false;
   for (var i = 0, len = radioInput.length; i < len; i++) {
     if (radioInput[i].checked) {
@@ -95,9 +105,13 @@ const submitForm = (e) => {
     }
   }
 
+  // Handle when the form is valid
   if (isValidText && isValidRadio) {
+    // Hide the inscription modal
     modalBg.style.display = "none";
+    // Show the confirmation modal
     alertBg.style.display = "block";
+    // Reset the form
     form.reset();
   }
 };
